97e0724df67a55c2d0e5453a8484496ad7e474e3
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2019-07-24 16:48:49 -0700

    Refactoriza usando ciclo for para convertir diccionario a renglón

--- app.py
+++ app.py
@@ -9,19 +9,10 @@ def index():
 @app.route('/api/v1/record', methods=['POST'])
 def add_new_record():
     datafile = 'data/testmake.log.csv'
-    repo = request.args['repo']
-    objetivo = request.args['objetivo']
-    revision = request.args['revision']
-    id = request.args['id']
-    phony = request.args['phony']
-    analista = request.args['analista']
-    maquina = request.args['maquina']
-    timestamp = request.args['timestamp']
-    es_make_exitoso = request.args['es_make_exitoso']
-    es_phony = request.args['es_phony']
-    existe_objetivo = request.args['existe_objetivo']
+    fin_linea = '\n'
     with open(datafile, 'a') as archivo:
-        archivo.write(f"{repo},{objetivo},{revision},{id},{phony},{analista},{maquina},{timestamp},{es_make_exitoso},{es_phony},{existe_objetivo}\n")
+        for indice, llave in enumerate(sorted(request.args.keys())):
+            archivo.write(f"{request.args[llave]}{',' if indice+1 < len(request.args) else fin_linea}")
     return jsonify(request.args)
 
 if __name__ == "__main__":
