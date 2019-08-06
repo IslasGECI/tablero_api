c4112e75b8dd7f6789874515b17f2ca90880ef81
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2019-08-06 12:09:48 -0700

    Elimina lo que ahora es responsabilidad de la API

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
-from flask import Flask, jsonify, render_template, request
-from tablero.Dashboard import get_dashboard
+from flask import Flask, jsonify, render_template
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html", tablero=get_dashboard())
-
-@app.route('/api/v1/record', methods=['POST'])
-def add_new_record():
-    datafile = 'data/testmake.log.csv'
-    with open(datafile, 'a') as archivo:
-        for indice, llave in enumerate(sorted(request.args.keys())):
-            archivo.write("{}{}".format(request.args[llave], "," if indice+1 < len(request.args) else "\n"))
-    return jsonify(request.args)
+    peticion = requests.get(url="http://localhost:500/api/v1/dashboard")
+    return render_template("index.html", tablero=peticion.json())
 
 if __name__ == "__main__":
-    tablero=get_dashboard()
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
