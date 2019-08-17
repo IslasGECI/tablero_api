05c785332d49ec2197da2160c8daf6a3b00c453d
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2019-08-16 19:58:22 -0700

    Refactoriza extrayendo función append_row_to_dashboard de get_dashboard

--- tablero/Dashboard.py
+++ tablero/Dashboard.py
@@ -6,23 +6,25 @@ import pandas as pd
 from .io import get_last_record_per_revision
 
 
-def is_default(tabla):
-    es_default = ((tabla.revision == "default") | (tabla.revision == "master"))
+def is_default(registro):
+    es_default = ((registro.revision == "default")
+                  | (registro.revision == "master"))
     return es_default
 
 
-def is_develop(tabla):
-    es_develop = ((tabla.revision == "develop") | (tabla.revision == "development"))
+def is_develop(registro):
+    es_develop = ((registro.revision == "develop") |
+                  (registro.revision == "development"))
     return es_develop
 
 
-def get_badge(tabla, es_rama):
+def get_badge(registro, es_rama):
     medalla_exito = "https://img.shields.io/badge/make-PASS-green.svg"
     medalla_fracaso = "https://img.shields.io/badge/make-FAIL-red.svg"
     medalla_na = "https://img.shields.io/badge/make-NA-lightgrey.svg"
     if not np.any(es_rama):
         medalla = medalla_na
-    elif tabla.exitoso.values[es_rama][-1] == 1:
+    elif registro.exitoso.values[es_rama][-1] == 1:
         medalla = medalla_exito
     else:
         medalla = medalla_fracaso
@@ -34,12 +36,17 @@ def get_dashboard():
     es_rama = is_develop(registro_ramas) | is_default(registro_ramas)
     tablero_ramas = registro_ramas[es_rama]
     tablero = pd.DataFrame(columns=['repo', 'objetivo', 'develop', 'default'])
-    for (repo, objetivo), tabla in tablero_ramas.groupby(by=["repo", "objetivo"]):
-        es_default = is_default(tabla)
-        es_develop = is_develop(tabla)
-        medalla_default = get_badge(tabla, es_default)
-        medalla_develop = get_badge(tabla, es_develop)
-        renglon_concatenar = {"repo": repo, "objetivo": objetivo,
-                              "develop": medalla_develop, "default": medalla_default}
-        tablero = tablero.append(renglon_concatenar, ignore_index=True)
+    for (repo, objetivo), registro in tablero_ramas.groupby(by=["repo", "objetivo"]):
+        tablero = append_row_to_dashboard(registro, repo, objetivo, tablero)
+    return tablero
+
+
+def append_row_to_dashboard(registro, repo, objetivo, tablero):
+    es_default = is_default(registro)
+    es_develop = is_develop(registro)
+    medalla_default = get_badge(registro, es_default)
+    medalla_develop = get_badge(registro, es_develop)
+    renglon_concatenar = {"repo": repo, "objetivo": objetivo,
+                          "develop": medalla_develop, "default": medalla_default}
+    tablero = tablero.append(renglon_concatenar, ignore_index=True)
     return tablero
