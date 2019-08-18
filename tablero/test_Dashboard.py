021112033c048e23acc579e46b60f9c97e9092ae
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2019-08-17 20:35:51 -0700

    Agrega prueba para Dashboard.is_develop()

--- tablero/test_Dashboard.py
+++ tablero/test_Dashboard.py
@@ -1,5 +1,6 @@
-from .Dashboard import is_default
 from collections import namedtuple
+
+from .Dashboard import *
 
 registro = namedtuple('registro', ['repo', 'objetivo', 'revision', 'exitoso'])
 
@@ -14,3 +15,14 @@ def test_is_default():
     assert default_es_default
     assert master_es_default
     assert not stable_es_default
+
+
+def test_is_develop():
+    dev_es_develop = is_develop(registro('repositorio', 'reporte', 'dev', 0))
+    develop_es_develop = is_develop(
+        registro('repositorio', 'reporte', 'develop', 0))
+    development_es_develop = is_develop(
+        registro('repositorio', 'reporte', 'development', 0))
+    assert not dev_es_develop
+    assert develop_es_develop
+    assert development_es_develop
