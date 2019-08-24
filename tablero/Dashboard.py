aab3139dc0708d3ac034eb8bde14928e1fffb81a
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2019-08-23 20:41:43 -0700

    Reordena funciones en formato cascada

--- tablero/Dashboard.py
+++ tablero/Dashboard.py
@@ -6,6 +6,29 @@ import pandas as pd
 from .io import get_last_record_per_revision
 
 
+def get_dashboard(log_name="data/testmake.log.csv"):
+    registro_ramas = get_last_record_per_revision(log_name)
+    es_rama = is_develop(registro_ramas) | is_default(registro_ramas)
+    tablero_ramas = registro_ramas[es_rama]
+    tablero = pd.DataFrame(columns=['repo', 'objetivo', 'develop', 'default'])
+    for (repo, objetivo), registros_agrupados in tablero_ramas.groupby(by=["repo", "objetivo"]):
+        tablero = append_row_to_dashboard(
+            registros_agrupados, repo, objetivo, tablero)
+    return tablero
+
+
+def is_develop(registro):
+    es_develop = ((registro.revision == "develop") |
+                  (registro.revision == "development"))
+    return es_develop
+
+
+def is_default(registro):
+    es_default = ((registro.revision == "default")
+                  | (registro.revision == "master"))
+    return es_default
+
+
 def append_row_to_dashboard(registros_agrupados, repo, objetivo, tablero):
     es_default = is_default(registros_agrupados)
     es_develop = is_develop(registros_agrupados)
@@ -15,18 +38,6 @@ def append_row_to_dashboard(registros_agrupados, repo, objetivo, tablero):
                           "develop": medalla_develop, "default": medalla_default}
     tablero = tablero.append(renglon_concatenar, ignore_index=True)
     return tablero
-
-
-def is_default(registro):
-    es_default = ((registro.revision == "default")
-                  | (registro.revision == "master"))
-    return es_default
-
-
-def is_develop(registro):
-    es_develop = ((registro.revision == "develop") |
-                  (registro.revision == "development"))
-    return es_develop
 
 
 def get_badge(registro, es_rama):
@@ -40,13 +51,3 @@ def get_badge(registro, es_rama):
     else:
         medalla = medalla_fracaso
     return medalla
-
-
-def get_dashboard(log_name="data/testmake.log.csv"):
-    registro_ramas = get_last_record_per_revision(log_name)
-    es_rama = is_develop(registro_ramas) | is_default(registro_ramas)
-    tablero_ramas = registro_ramas[es_rama]
-    tablero = pd.DataFrame(columns=['repo', 'objetivo', 'develop', 'default'])
-    for (repo, objetivo), registros_agrupados in tablero_ramas.groupby(by=["repo", "objetivo"]):
-        tablero = append_row_to_dashboard(registros_agrupados, repo, objetivo, tablero)
-    return tablero
