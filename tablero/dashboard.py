ce1ab883ab2293b3bb461f770fb08e7746a6c01b
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2019-09-07 14:13:48 -0700

    Crea función 'get_row_to_append'

--- tablero/dashboard.py
+++ tablero/dashboard.py
@@ -24,20 +24,25 @@ def is_develop(registro):
 
 
 def is_default(registro):
-    es_default = ((registro.revision == "default")
-                  | (registro.revision == "master"))
+    es_default = ((registro.revision == "default") |
+                  (registro.revision == "master"))
     return es_default
 
 
 def append_row_to_dashboard(registros_agrupados, repo, objetivo, tablero):
+    renglon_concatenar = get_row_to_append(registros_agrupados, repo, objetivo)
+    tablero = tablero.append(renglon_concatenar, ignore_index=True)
+    return tablero
+
+
+def get_row_to_append(registros_agrupados, repo, objetivo):
     es_default = is_default(registros_agrupados)
     es_develop = is_develop(registros_agrupados)
     medalla_default = get_badge(registros_agrupados, es_default)
     medalla_develop = get_badge(registros_agrupados, es_develop)
     renglon_concatenar = {"repo": repo, "objetivo": objetivo,
                           "develop": medalla_develop, "default": medalla_default}
-    tablero = tablero.append(renglon_concatenar, ignore_index=True)
-    return tablero
+    return renglon_concatenar
 
 
 def get_badge(registro, es_rama):
