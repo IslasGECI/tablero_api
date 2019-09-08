3a17af78c33ea4040728ae3f08887ff30a5bfbce
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2019-09-08 13:36:45 -0700

    Agrega prueba para función 'import_data()'

--- tests/test_read_records.py
+++ tests/test_read_records.py
@@ -1,5 +1,7 @@
 from tablero.io import get_last_record_per_revision
 
+from tablero.io.read_records import import_data
+
 
 def test_count_of_last_record_per_revision():
     registros = get_last_record_per_revision(
@@ -12,6 +14,14 @@ def test_count_of_last_record_per_revision():
 def test_columns_of_last_record_per_revision():
     registros = get_last_record_per_revision(
         log_name="data/testmake.test.csv")
-    columns_of_records_expected = sorted(['repo', 'objetivo', 'revision', 'exitoso'])
+    columns_of_records_expected = sorted(
+        ['repo', 'objetivo', 'revision', 'exitoso'])
     columns_of_records_obtained = sorted(registros)
     assert columns_of_records_obtained == columns_of_records_expected
+
+
+def test_imported_data():
+    registros_importados = import_data(log_name="data/testmake.test.csv")
+    assert sum(registros_importados.exito == 0) == 197
+    assert sum(registros_importados.exito == 1) == 156
+    assert len(registros_importados.exito) == 353
