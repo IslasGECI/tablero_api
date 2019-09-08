f789416db02603f954950cb7eca8e7209b145527
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2019-09-08 12:53:46 -0700

    Verifica que las columnas en el registro son las esperadas

--- tests/test_read_records.py
+++ tests/test_read_records.py
@@ -2,8 +2,16 @@ from tablero.io import get_last_record_per_revision
 
 
 def test_count_of_last_record_per_revision():
-    registro_ramas = get_last_record_per_revision(
+    registros = get_last_record_per_revision(
         log_name="data/testmake.test.csv")
-    count_of_revisions_expected = 52
-    count_of_revisions_obtained = len(registro_ramas.index)
-    assert count_of_revisions_obtained == count_of_revisions_expected
+    count_of_records_expected = 52
+    count_of_records_obtained = len(registros.index)
+    assert count_of_records_obtained == count_of_records_expected
+
+
+def test_columns_of_last_record_per_revision():
+    registros = get_last_record_per_revision(
+        log_name="data/testmake.test.csv")
+    columns_of_records_expected = sorted(['repo', 'objetivo', 'revision', 'exitoso'])
+    columns_of_records_obtained = sorted(registros)
+    assert columns_of_records_obtained == columns_of_records_expected
