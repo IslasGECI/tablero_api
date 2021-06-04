c57e29077eaa4c626a6f25a5b295e7ae81178f2a
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-06-04 14:38:57 -0700

    🔥 Delete unused tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,26 +1,12 @@
 from ..api import app
-import flask
 from flask_testing import TestCase
 import filecmp
-
-
-class Request:
-    def __init__(self):
-        self.arg = {"analista": "inspector", "repo": "repo_1"}
-
-
-def test_add_new_record(mocker):
-    app.post("/api/v1/records", data=dict(analista="inspecto", repo="repo_1"))
-    app.get("/api/v1/records", query_string=dict(arg1="data1", arg2="data2"))
-    with app.test_request_context("/?analista=inspecto"):
-        assert flask.request.args["analista"] == "inspecto"
 
 
 class TestApp(TestCase):
     def create_app(self):
         """Flask knows that is in testing mode"""
         app.config["TESTING"] = True
-
         return app
 
     def test_add_new_record(self):
