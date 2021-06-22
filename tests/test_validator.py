957daca2ad8dcfd117133d9d47544fa00448c777
Xavier Paredes <xavier.paredes@Islas.org.mx>
2021-06-22 12:18:28 -0700

    ✅ Pass test

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,2 +1,13 @@
+from tablero.request_validator.validator import is_valid_user
+
+
+class Request:
+    def __init__(self):
+        self.args = {"analista": "inspector"}
+
+
 def test_is_valid_user():
-    assert False
+    request = Request()
+    obtained_is_valid_analista = is_valid_user(request)
+    expected_is_valid_analista = False
+    assert obtained_is_valid_analista == expected_is_valid_analista
