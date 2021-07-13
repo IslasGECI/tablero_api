681f1a48e46746499d22a09380d44fe273817095
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-07-13 12:51:27 -0700

    ✅🧹 Assess that valid analyst works

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -2,11 +2,14 @@ from tablero.request_validator.validator import is_valid_user
 
 
 class Request:
-    def __init__(self):
-        self.args = {"analista": "inspector"}
+    def __init__(self, analista):
+        self.args = {"analista": analista}
 
 
 def test_is_valid_user():
-    request = Request()
-    is_valid_analista = is_valid_user(request)
+    not_valid_request = Request("inspector")
+    is_valid_analista = is_valid_user(not_valid_request)
     assert not is_valid_analista
+    valid_request = Request("no_inspector")
+    is_valid_analista = is_valid_user(valid_request)
+    assert is_valid_analista
