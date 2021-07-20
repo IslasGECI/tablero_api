3a36595511ed1cd01ab7f92c843d69a3cfe219c1
Xavier Paredes <jxavierpp1@gmail.com>
2021-07-20 15:57:58 -0700

    ✏️ Fix typo

--- tests/test_app.py
+++ tests/test_app.py
@@ -29,5 +29,5 @@ class TestApp(TestCase):
         self.assert200(get_something)
 
     def _was_added_a_new_row(self, analista, token):
-        self.client.post(f"/api/v1/records?analista={analista}", headers={"Autorization": token})
+        self.client.post(f"/api/v1/records?analista={analista}", headers={"Authorization": token})
         return not filecmp.cmp("data/testmake.log.tests.csv", "data/testmake.log.csv")
